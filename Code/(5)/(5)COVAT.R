ba75a9c2313778ae03be4fc2a8837f415e0d57e5
Parsa Yousefi <prs.yusefi@gmail.com>
2024-02-06 15:21:59 +0100

    ID(5) adjusted code

--- Code/(5)/(5)COVAT.R
+++ Code/(5)/(5)COVAT.R
@@ -1,12 +1,12 @@
-# Data from Fig3:
-placebo_means <- c(LVF_100 = 55.319, RVF_100 = 42.553, LVF_800 = 19.149, RVF_800 = 36.170)
-placebo_whiskers <- c(LVF_100 = 87.234, RVF_100 = 82.979, LVF_800 = 74.468, RVF_800 = 89.362)
-psilocybin_means <- c(LVF_100 = 113.380, RVF_100 = 107.042, LVF_800 = 3.521, RVF_800 = 39.437)
-psilocybin_whiskers <- c(LVF_100 = 180.986, RVF_100 = 193.662, LVF_800 = 83.803, RVF_800 = 111.268)
+# Data from Fig2:
+predrug_means <- c(LVF_100 = 55.319, RVF_100 = 42.553, LVF_800 = 19.149, RVF_800 = 36.170)
+predrug_whiskers <- c(LVF_100 = 87.234, RVF_100 = 82.979, LVF_800 = 74.468, RVF_800 = 89.362)
+postdrug_means <- c(LVF_100 = 113.380, RVF_100 = 107.042, LVF_800 = 3.521, RVF_800 = 39.437)
+postdrug_whiskers <- c(LVF_100 = 180.986, RVF_100 = 193.662, LVF_800 = 83.803, RVF_800 = 111.268)
 
 # Calculate the SDs
-placebo_sds <- placebo_whiskers - placebo_means
-psilocybin_sds <- psilocybin_whiskers - psilocybin_means
+predrug_sds <- predrug_whiskers - predrug_means
+postdrug_sds <- postdrug_whiskers - postdrug_means
 
 # Function to calculate pooled SD
 pooled_sd <- function(sd1, sd2) {
@@ -19,18 +19,18 @@ cohens_d <- function(mean1, mean2, sd1, sd2) {
 }
 
 # Calculate Cohen's d for each SOA, averaged across LVF and RVF
-cohens_d_100 <- (cohens_d(placebo_means["LVF_100"], psilocybin_means["LVF_100"], placebo_sds["LVF_100"], psilocybin_sds["LVF_100"]) +
-                   cohens_d(placebo_means["RVF_100"], psilocybin_means["RVF_100"], placebo_sds["RVF_100"], psilocybin_sds["RVF_100"])) / 2
+cohens_d_100 <- (cohens_d(predrug_means["LVF_100"], postdrug_means["LVF_100"], predrug_sds["LVF_100"], postdrug_sds["LVF_100"]) +
+                   cohens_d(predrug_means["RVF_100"], postdrug_means["RVF_100"], predrug_sds["RVF_100"], postdrug_sds["RVF_100"])) / 2
 
-cohens_d_800 <- (cohens_d(placebo_means["LVF_800"], psilocybin_means["LVF_800"], placebo_sds["LVF_800"], psilocybin_sds["LVF_800"]) +
-                   cohens_d(placebo_means["RVF_800"], psilocybin_means["RVF_800"], placebo_sds["RVF_800"], psilocybin_sds["RVF_800"])) / 2
+cohens_d_800 <- (cohens_d(predrug_means["LVF_800"], postdrug_means["LVF_800"], predrug_sds["LVF_800"], postdrug_sds["LVF_800"]) +
+                   cohens_d(predrug_means["RVF_800"], postdrug_means["RVF_800"], predrug_sds["RVF_800"], postdrug_sds["RVF_800"])) / 2
 
 # Calculate Cohen's d averaged across SOAs
 cohens_d_overall_soa <- (cohens_d_100 + cohens_d_800) / 2
 
 # Calculate one overall Cohen's d
-mean_differences <- psilocybin_means - placebo_means
-pooled_sds <- mapply(pooled_sd, placebo_sds, psilocybin_sds)
+mean_differences <- postdrug_means - predrug_means
+pooled_sds <- mapply(pooled_sd, predrug_sds, postdrug_sds)
 overall_cohens_d <- sum(mean_differences / pooled_sds) / length(mean_differences)
 
 # results
